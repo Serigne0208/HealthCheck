15f4d49213dc6cbd3146042ed348f4e0d60b7c5e
serigne-saliou sow <saliousow0208@gmail.com>
2024-06-25 18:17:39 +0200

    commit: test git

--- healthcheck.client/src/app/app.module.ts
+++ healthcheck.client/src/app/app.module.ts
@@ -34,4 +34,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  //TEST git
+}
